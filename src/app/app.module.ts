0b8829c201b1d415820737b81a785acb4aba4586
Stephan Bartkowiak <stephan@ecobonuz.com>
2019-09-17 23:44:51 -0300

    Updating Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarLeftComponent } from './components/sidebar-left/sidebar-left.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { ROUTES } from './app.routes';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { BarbersComponent } from './pages/barbers/barbers.component';
+import { AppoinmentComponent } from './pages/appoinment/appoinment.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MidlebarComponent } from './components/midlebar/midlebar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarLeftComponent,
+    FooterComponent,
+    MainComponent,
+    AboutComponent,
+    ServicesComponent,
+    BarbersComponent,
+    AppoinmentComponent,
+    ContactComponent,
+    LoginComponent,
+    MidlebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
