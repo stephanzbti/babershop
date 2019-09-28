9b7d4bdf28f9010fe8374482960fdcc67cc997c1
Stephan Bartkowiak <stephan@ecobonuz.com>
2019-09-28 15:45:41 -0300

    Correct Login page to work with LocalStorage

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginUser() {
+    window.localStorage.setItem('permission','account')
+  }
+
   constructor() { }
 
   ngOnInit() {
