412c1e82c3c753fa09beaf0018cd811ee879802e
Stephan Bartkowiak <stephan@ecobonuz.com>
2019-09-28 15:46:03 -0300

    Creating Permissions in Menu

--- src/app/components/sidebar-left/sidebar-left.component.ts
+++ src/app/components/sidebar-left/sidebar-left.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar-left.component.css']
 })
 export class SidebarLeftComponent implements OnInit {
+  perm:any
 
-  constructor() { }
+  permission: any
+
+  constructor() { 
+    this.perm = localStorage.getItem('permission')
+    console.log(this.perm)
+  }
 
   ngOnInit() {
+    this.permission = {
+      login: this.perm == 'public' ? true : false,
+      about: this.perm == 'public' ? true : false,
+      service: this.perm == 'public' ? true : false,
+      barber: this.perm == 'account' ? true : false,
+      appointment: this.perm == 'account' ? true : false,
+      contact: this.perm == 'public' ? true : false,
+      account: this.perm == 'account' ? true : false,
+      dashboard: this.perm == 'admin' ? true : false,
+      logout: this.perm == 'account' ? true : false
+    }
   }
 
 }
